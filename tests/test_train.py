425eaa3b373254698b859d40af34c67d68dec7da
Juan Julio Montreuil Berrocal <72266908+JuanMontreuil08@users.noreply.github.com>
2025-10-26 12:43:45 -0500

    Update test_train.py

--- tests/test_train.py
+++ tests/test_train.py
@@ -2,4 +2,4 @@ from train import train_and_eval
 
 def test_training_runs():
     metrics = train_and_eval()
-    assert "accuracy" in metrics and metrics["accuracy"] > 0.5
+    assert "recall" in metrics and metrics["recall"] > 0.5
